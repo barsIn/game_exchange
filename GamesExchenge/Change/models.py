7bc396676f109b7639f5575f8f9677700a50e0a4
barsIn <bars_in@mail.ru>
2024-02-12 18:45:39 +0700

    Migrations fixed

--- GamesExchenge/Change/models.py
+++ GamesExchenge/Change/models.py
@@ -76,7 +76,7 @@ class Review(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.text
+        return f'{self.text}'
 
 # Модель чата
 class Chat(models.Model):
